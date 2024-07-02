7cfd3f95a0016988d4d7ac82f56d27aa1016d443
Amritesh Kumar <157412986+AmriteshKr24@users.noreply.github.com>
2024-07-02 06:26:20 +0530

    abcd

--- server/index.js
+++ server/index.js
@@ -1,19 +1,21 @@
 require("dotenv").config();
-const express = require('express');
-const {connectToMongoDB} = require('./database');
-const path = require('path');
+const express = require("express");
+const { connectToMongoDB } = require("./database");
+const path = require("path");
+
 const app = express();
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "build")));
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-// Import the routes
-const router = require('./routes');
-app.use('/api', router);
+    res.sendFile(path.join(__dirname, "build/index.html"));
+})
 
-const port=process.env.PORT||4000;
+
+const router = require("./routes");
+app.use("/api", router);
+
+const port = process.env.PORT || 5000;
 
 async function startServer() {
     await connectToMongoDB();
